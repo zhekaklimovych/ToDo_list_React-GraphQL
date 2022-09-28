8332bd62de27d6acbb52fee10f60b76d5b8ca372
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-09-28 12:37:47 +0300

    add awssync, create client

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink} from '@apollo/client';
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
+
+import appSyncConfig from './aws-exports';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createAuthLink} from "aws-appsync-auth-link";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const url = appSyncConfig.aws_appsync_graphqlEndpoint;
+const region = appSyncConfig.aws_appsync_region;
+const auth = {
+    type: appSyncConfig.aws_appsync_authenticationType,
+    apiKey: appSyncConfig.aws_appsync_apiKey
+};
+const httpLink = new HttpLink({ uri: url });
+const link = ApolloLink.from([
+    createAuthLink({ url, region, auth }),
+    createSubscriptionHandshakeLink({ url, region, auth }, httpLink),
+]);
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+});
+
 root.render(
     <React.StrictMode>
-        <App/>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
