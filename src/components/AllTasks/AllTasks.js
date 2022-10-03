8075b85cbc57ee1004197e1fbff2a652928bf52a
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-10-03 14:35:04 +0300

    add pagination and add refetch to data

--- src/components/AllTasks/AllTasks.js
+++ src/components/AllTasks/AllTasks.js
@@ -10,13 +10,29 @@ import './AllTasks.css'
 
 const AllTasks = () => {
 
-    let {data, loading, error} = useQuery(GET_ALL_TASKS);
+    let {data, loading, error, refetch} = useQuery(GET_ALL_TASKS);
 
     const [tasks, setTasks] = useState([]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const tasksNumber = 3;
+
+    let skip = tasksNumber * pageNumber - tasksNumber;
+    let maxElements = skip + tasksNumber;
+    let paginetedTasks = tasks?.length == 0 ? [] : tasks?.slice(skip, maxElements);
+
+    const handlePrev =()=> {
+        if(pageNumber === 1) return;
+        setPageNumber(pageNumber - 1);
+    }
+
+    const handleNext = () => {
+        setPageNumber(prev=> prev + 1);
+    }
 
     useEffect(()=> {
         if(data) {
             setTasks(data?.allTasks?.items);
+            refetch();
         }
     }, [data]);
 
@@ -33,8 +49,12 @@ const AllTasks = () => {
     return(
         <div className="tasks-container">
             <h1>Tasks</h1>
-            {tasks.map(item => <Task key={item.id} data={item}/>)}
-
+            {paginetedTasks.map(item => <Task key={item.id} data={item}/>)}
+            <div>Page {pageNumber} </div>
+            <div>
+                <button disabled={pageNumber === 1} className="shine-button" onClick={handlePrev} >prev</button>
+                <button disabled={Math.ceil(tasks.length / tasksNumber) === pageNumber} className="shine-button" onClick={handleNext}>next</button>
+            </div>
             <NavLink to={'/'} className="shine-button">Back</NavLink>
 
         </div>
